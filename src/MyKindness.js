28bd5ac4bf61059f747a02597a6ecd0546f732ed
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-06-30 21:32:33 -0400

    Modifies the backgroun of MyKindness page

--- src/MyKindness.js
+++ src/MyKindness.js
@@ -3,6 +3,12 @@ import { useEffect, useState } from "react";
 
 function MyKindness({ currentUser, handleClick, dataChange }) {
 
+    const backgroundStyle = {
+        backgroundColor: "#ffb6f0",
+        height: "100vh",
+        backgroundSize: "cover",
+    }
+
     // using a state of an Array of my kindness acts (posts)
     const [myPostsArray, setMyPostsArray] = useState([])
     //Getting all my kindness acts from the server
@@ -23,7 +29,7 @@ function MyKindness({ currentUser, handleClick, dataChange }) {
     const postList = myPostsArray.map(user => <Post key={user.id} act={user.act} title={user.title} user={user.user} like={user.like} IDidIt={user.IDidIt} IWillDoIt={user.IWillDoIt} id={user.id} handleClick={handleClick} />)
 
     return (
-        <div className="column is-offset-2 is-8">
+        <div className="column is-offset-2 is-8" style={backgroundStyle}>
             {postList}
         </div>
     )
