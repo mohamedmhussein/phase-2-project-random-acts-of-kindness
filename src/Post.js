7a62fbe0aa43ab3d3b4e6f388357f907d803cc8b
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-07-02 16:41:38 -0400

    Removes the delete button and adjusts the margin for the title in the post component

--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,10 @@
 function Post({ act, title, user, like, IDidIt, IWillDoIt, id, handleClick }) {
     const buttonClasses = "button is-warning has-text-weight-semibold is-rounded"
-    
+
     return (
         <div className="columns my-6 ">
             <div className="box column mx-6 content is-normal " style={{ opacity: "195%" }}>
-                <button className="column delete is-offset-11  is-1 mt-4"></button>
-                <h1 className="my-1 has-text-centered is-6 "> {title} </h1>
+                <h1 className="mt-5 mb-3 has-text-centered is-6 "> {title} </h1>
                 {{ user } ? <h4 className="my-1 has-text-centered is-6 has-text-weight-light	"> By: {user} </h4> : {}}
                 <p className="column mx-4 has-text-justified is-size-6" style={{ height: "15em", overflowY: "scroll" }}>
                     {act}
